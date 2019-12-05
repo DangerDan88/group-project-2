c7bc6c09e94b2d2700679f65554766d9685821c8
Danger <51889207+DangerDan88@users.noreply.github.com>
2019-12-05 13:12:45 -0700

    merge

--- config/config.js
+++ config/config.js
@@ -2,22 +2,27 @@ require("dotenv").config();
 
 module.exports = {
   "development": {
-    "username": process.env.USER,
-    "password": process.env.PASSWORD,
-    "database": process.env.DATABASE,
-    "host": process.env.HOST,
-    "dialect": "mysql"
-  },
-  "test": {
     "username": "root",
     "password": "yourRootPassword",
     "database": "game",
     "host": "localhost",
+    "dialect": "mysql"
+   
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
     "dialect": "mysql",
-    "logging": false
+    "operatorsAliases": false
   },
   "production": {
-    "use_env_variable": "JAWSDB_URL",
-    "dialect": "mysql"
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
   }
 };
